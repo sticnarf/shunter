b03abb08706742611a280824b71aaca4ddfd3b36
Yilin Chen <sticnarf@gmail.com>
2017-07-06 23:25:09 +0800

    reply to client

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,14 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
-use futures::{Stream, Future, BoxFuture, future};
+use futures::{Stream, Future, BoxFuture, Async, future};
 use tokio_core::reactor::{Core, Handle};
 use tokio_core::net::{TcpStream, TcpListener};
 use tokio_io::io::{read_exact, write_all};
+use tokio_io::AsyncRead;
 use std::net::{SocketAddr, IpAddr, Ipv4Addr, Ipv6Addr};
-use std::str;
-use std::str::FromStr;
-use std::io;
-use std::io::ErrorKind;
+use std::str::{self, FromStr};
+use std::io::{self, Read, Write, ErrorKind};
 use num::FromPrimitive;
 
 fn main() {
@@ -45,9 +44,9 @@ fn serve(socket: TcpStream, peer_address: SocketAddr, handle: Handle) -> Box<Fut
     }).and_then(|(socket, method_cnt)| {
         read_exact(socket, vec![0u8; method_cnt as usize]).and_then(|(socket, buf)| {
             if buf.iter().any(|&x| x == NO_AUTHENTICATION_REQUIRED) {
-                mybox(write_all(socket, [SOCKS5_VERSION, NO_AUTHENTICATION_REQUIRED]).map(|(socket, _)| socket))
+                non_send_box(write_all(socket, [SOCKS5_VERSION, NO_AUTHENTICATION_REQUIRED]).map(|(socket, _)| socket))
             } else {
-                mybox(write_all(socket, [SOCKS5_VERSION, NO_ACCEPTABLE_METHODS]).and_then(|_| Err(io::Error::new(ErrorKind::Other, "Unsupported SOCKS version"))))
+                non_send_box(write_all(socket, [SOCKS5_VERSION, NO_ACCEPTABLE_METHODS]).and_then(|_| Err(io::Error::new(ErrorKind::Other, "Unsupported SOCKS version"))))
             }
         })
     });
@@ -70,12 +69,12 @@ fn serve(socket: TcpStream, peer_address: SocketAddr, handle: Handle) -> Box<Fut
         })
     }).and_then(|(socket, aytp)| {
         match aytp {
-            AYTP::IPv4 => mybox(read_exact(socket, [0u8; 6]).map(|(socket, buf)| {
+            AYTP::IPv4 => non_send_box(read_exact(socket, [0u8; 6]).map(|(socket, buf)| {
                 let ip = IpAddr::from(Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]));
                 let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
                 (socket, SocketAddr::new(ip, port))
             })),
-            AYTP::IPv6 => mybox(read_exact(socket, [0u8; 18]).map(|(socket, buf)| {
+            AYTP::IPv6 => non_send_box(read_exact(socket, [0u8; 18]).map(|(socket, buf)| {
                 let ip = IpAddr::from(Ipv6Addr::new(
                     (buf[0] as u16) << 8 | (buf[1] as u16), (buf[2] as u16) << 8 | (buf[3] as u16),
                     (buf[4] as u16) << 8 | (buf[5] as u16), (buf[6] as u16) << 8 | (buf[7] as u16),
@@ -84,7 +83,7 @@ fn serve(socket: TcpStream, peer_address: SocketAddr, handle: Handle) -> Box<Fut
                 let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
                 (socket, SocketAddr::new(ip, port))
             })),
-            AYTP::DomainName => mybox(read_exact(socket, [0u8]).and_then(|(socket, buf)| {
+            AYTP::DomainName => non_send_box(read_exact(socket, [0u8]).and_then(|(socket, buf)| {
                 read_exact(socket, vec![0u8; (buf[0] as usize) + 2]).and_then(|(socket, buf)| {
                     // TODO: Resolve IP from DNS server
                     Ok((socket, SocketAddr::from_str("127.0.0.1:80").unwrap()))
@@ -94,13 +93,41 @@ fn serve(socket: TcpStream, peer_address: SocketAddr, handle: Handle) -> Box<Fut
     });
 
     let reply = req.and_then(move |(socket, socket_addr)| {
-        TcpStream::connect(&socket_addr, &handle).then(|conn| {
-            // TODO: Connect to remote server
-            Ok(())
+        TcpStream::connect(&socket_addr, &handle).then(move |res| {
+            info!("Connecting {}", socket_addr);
+            let mut reply_data = vec![SOCKS5_VERSION, SUCCEEDED_REPLY, RESERVED_CODE];
+            match socket_addr {
+                SocketAddr::V4(addr) => {
+                    let ip = addr.ip().octets();
+                    let port = addr.port();
+                    reply_data.extend_from_slice(&[AYTP::IPv4 as u8, ip[0], ip[1], ip[2], ip[3], (port >> 8) as u8, port as u8])
+                },
+                SocketAddr::V6(addr) => {
+                    let ip = addr.ip().octets();
+                    let port = addr.port();
+                    reply_data.extend_from_slice(&[AYTP::IPv6 as u8, ip[0], ip[1], ip[2], ip[3], ip[4], ip[5], ip[6], ip[7],
+                        ip[8], ip[9], ip[10], ip[11], ip[12], ip[13], ip[14], ip[15],
+                        (port >> 8) as u8, port as u8]);
+                }
+            };
+            match res {
+                Ok(conn) => {
+                    non_send_box(write_all(socket, reply_data).and_then(|(socket, buf)| Ok((socket, conn))))
+                },
+                Err(e) => {
+                    reply_data[1] = GENERAL_SOCKS_SERVER_FAILURE_REPLY;
+                    non_send_box(write_all(socket, reply_data).and_then(|_| Err(io::Error::new(ErrorKind::Other, "Connection failure"))))
+                }
+            }
         })
     });
 
-    mybox(reply.then(move |res| {
+    let passing = reply.and_then(|(socket, conn)| {
+        info!("Ready for passing!");
+        Ok(())
+    });
+
+    non_send_box(passing.then(move |res| {
         if let Err(e) = res {
             info!("Error with client {}: {}", peer_address, e);
         }
@@ -108,7 +135,7 @@ fn serve(socket: TcpStream, peer_address: SocketAddr, handle: Handle) -> Box<Fut
     }))
 }
 
-fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item=F::Item, Error=F::Error>> {
+fn non_send_box<F: Future + 'static>(f: F) -> Box<Future<Item=F::Item, Error=F::Error>> {
     Box::new(f)
 }
 
@@ -128,3 +155,6 @@ const NO_ACCEPTABLE_METHODS: u8 = 0xFF;
 const CONNECT_CMD: u8 = 0x01;
 
 const RESERVED_CODE: u8 = 0x00;
+
+const SUCCEEDED_REPLY: u8 = 0x00;
+const GENERAL_SOCKS_SERVER_FAILURE_REPLY: u8 = 0x01;
